7d9dbb30198dd69a1c4e2d4183fea908798cc053
gabrielgermanoraphael-del <gabrielgermanoraphael@gmail.com>
2026-05-30 21:33:14 -0300

    enviando arquivos java, com o model iniciado

---
+++ src/main/java/edu/br/fiec/model/Clientes.java
@@ -0,0 +1,37 @@
+package edu.br.fiec.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@Table(name = "cliente")
+public class Clientes {
+
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String nome;
+
+    private Long cpf;
+
+    private String email;
+
+    private  String telefone;
+
+    private String endereco;
+
+
+}
---
+++ src/main/java/edu/br/fiec/model/Pedidos.java
@@ -0,0 +1,33 @@
+package edu.br.fiec.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+
+
+@Table(name = "pedidos")
+public class Pedidos {
+
+    @id
+    @GeneratedValue
+    private Integer id;
+
+    private Double preco_total;
+
+    private Integer quantidade;
+
+    private Clientes cliente_id;
+
+    private String observacao;
+
+
+}
