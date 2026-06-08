31f63dcfbf18137055020c3953f3fd6825a990ad
46122 <joao46122@fiec.edu.br>
2026-06-08 19:55:54 -0300

    Commitando os DTOS

--- src/main/java/edu/br/fiec/controller/FloriculturaController.java
+++ src/main/java/edu/br/fiec/controller/FloriculturaController.java
@@ -1,4 +1,21 @@
 package edu.br.fiec.controller;
 
+import edu.br.fiec.service.FloriculturaService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Data
+@Controller
+@NoArgsConstructor
+@AllArgsConstructor
+
+
 public class FloriculturaController {
+
+    private final FloriculturaService floriculturaService;
+
+
+
 }
---
+++ src/main/java/edu/br/fiec/model/dto/PedidosDTO.java
@@ -0,0 +1,13 @@
+package edu.br.fiec.model.dto;
+
+public class PedidosDTO {
+
+    private Double preco_total;
+
+    private Integer quantidade;
+
+    private Integer cliente_id;
+
+    private String observacao;
+
+}
